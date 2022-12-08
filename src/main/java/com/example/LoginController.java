a9d85099941e024a544f5f088eff1f4f8268186f
yeseul <yeseul0900@handong.ac.kr>
2022-12-09 00:24:12 +0900

    login controller

---
+++ src/main/java/com/example/LoginController.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+    @Autowired
+    UserDAO.UserServiceImpl service;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(String t, Model model){
+        return "login";
+    }
+    @RequestMapping(value = "/loginOK", method = RequestMethod.POST)
+    public String loginCheck(HttpSession session, UserVO vo){
+        String returnURL= "";
+        if(session.getAttribute("login") != null){
+            session.removeAttribute("login");
+        }
+
+        UserVO loginvo = service.getUser(vo);
+        if(loginvo != null){
+            System.out.println("로그인 성공");
+            session.setAttribute("login", loginvo);
+            returnURL = "redirect:/board/list";
+        }else{
+            System.out.println("로그인 실패");
+            returnURL = "redirect:/login/login";
+        }
+        return returnURL;
+    }
+    @RequestMapping(value = "/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect/login/login";
+    }
+}
---
+++ src/main/java/com/example/UserVO.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class UserVO {
+    private String userid;
+    private String password;
+    private String username;
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+    public String getUserid(){
+        return userid;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+}
